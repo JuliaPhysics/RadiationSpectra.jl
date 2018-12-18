96375d58efb8a586a47e683e4f16f466796cb74a
Lukas Hauertmann <lhauert@mpp.mpg.de>
2018-12-18 15:45:56 +0100

    Update docs/make.jl due to change in Documenter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,6 @@ using RadiationSpectra
 makedocs(
     sitename = "RadiationSpectra.jl",
     modules = [RadiationSpectra],
-    format = :html,
     pages=[
         "Home" => "index.md",
         "Manual" => Any[
@@ -22,8 +21,7 @@ makedocs(
         "LICENSE" => "LICENSE.md",
     ],
     doctest = ("fixdoctests" in ARGS) ? :fix : true,
-    html_prettyurls = !("local" in ARGS),
-    html_canonical = "https://JuliaHEP.github.io/RadiationSpectra.jl/stable/",
+    format = Documenter.HTML(canonical = "https://JuliaHEP.github.io/RadiationSpectra.jl/stable/", prettyurls = !("local" in ARGS))
 )
 
 deploydocs(
