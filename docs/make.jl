c7454a9f7801dac920d975caf0fd7a7f85d40417
Lukas Hauertmann <lhauert@mpp.mpg.de>
2019-04-09 18:21:39 +0200

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -26,9 +26,41 @@ makedocs(
         "LICENSE" => "LICENSE.md",
     ],
     doctest = ("fixdoctests" in ARGS) ? :fix : true,
-    format = Documenter.HTML(canonical = "https://JuliaHEP.github.io/RadiationSpectra.jl/stable/", prettyurls = !("local" in ARGS))
+    format = Documenter.HTML(   canonical = "https://JuliaHEP.github.io/RadiationSpectra.jl/stable/", 
+                                prettyurls = !("local" in ARGS) 
+    )
 )
 
 deploydocs(
-    repo = "github.com/JuliaHEP/RadiationSpectra.jl.git"
+    repo = "github.com/JuliaHEP/RadiationSpectra.jl.git",
 )
+pick 858ff93 Updates make.jl for plots in documentation
+s 71110b3 Adds Plots.jl & PyPlot.jl to docs/project.toml
+s 29835d6 Fixes typo
+s fab5d2e Updates make.jl of docs
+s 5c51944 Updates make.jl format to Documenter@v0.21
+s 75dc538 Add import Base.in
+s 998a6fe Adds documentation
+s b3c05f7 Adds more documentation
+s 6c9115a Set docs `devbranch` to "dev"
+s 04421ba Docs: Sets devbranch to "master" and devurl to "master"
+
+pick 858ff93 Updates make.jl for plots in documentation
+pick de78acd Adds new simple detector type `CGD` (Cuboid Germanium Detector) for cartesian coordinates. Also,o adds CartesianGrid3D method for CGD
+pick 71110b3 Adds Plots.jl & PyPlot.jl to docs/project.toml
+pick 29835d6 Fixes typo
+pick fab5d2e Updates make.jl of docs
+pick 5c51944 Updates make.jl format to Documenter@v0.21
+pick d08f6fa further work on cart. coords
+pick 75dc538 Add import Base.in
+pick 998a6fe Adds documentation
+pick 4b93f7f updates doc
+pick b3c05f7 Adds more documentation
+pick 6c9115a Set docs `devbranch` to "dev"
+pick 04421ba Docs: Sets devbranch to "master" and devurl to "master"
+
+
+pick de78acd Adds new simple detector type `CGD` (Cuboid Germanium Detector) for cartesian coordinates. Also,o adds CartesianGrid3D method for CGD
+s d08f6fa further work on cart. coords
+s 4b93f7f updates doc
+
--- src/Fitting/plotrecipes.jl
+++ src/Fitting/plotrecipes.jl
@@ -1,7 +1,7 @@
-@recipe function f(fit::FitFunction; npoints = 501, use_initial_parameters = false)
-    x = collect(range(fit.fitrange[1], stop=fit.fitrange[2], length=npoints))
-    par = use_initial_parameters ? fit.initial_parameters : fit.parameters
-    y = fit.model(x, par)
+@recipe function f(ff::FitFunction; npoints = 501, use_initial_parameters = false)
+    x = collect(range(ff.fitrange[1], stop=ff.fitrange[2], length=npoints))
+    par = use_initial_parameters ? ff.initial_parameters : ff.parameters
+    y = ff.model(x, par)
     linecolor --> :red
     x,y
 end
