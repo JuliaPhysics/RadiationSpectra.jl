8158996c65e2f9051dc4622ad14f02cd226fdd11
Lukas Hauertmann <lhauert@mpp.mpg.de>
2019-04-19 22:46:00 +0200

    Refactor struct FitFunction

--- src/Fitting/lsqfit.jl
+++ src/Fitting/lsqfit.jl
@@ -1,49 +1,53 @@
-function lsqfit!(fit::FitFunction, xdata::Array{<:Real, 1}, ydata::Array{<:Real, 1}; estimate_uncertainties=false)::Nothing
-    fr = LsqFit.curve_fit(fit.model, Float64.(xdata), Float64.(ydata), collect(Float64, fit.initial_parameters))
-    fit.parameters = typeof(fit.initial_parameters)(fr.param)
-    if estimate_uncertainties fit.uncertainties = LsqFit.margin_error(fr, 1 - fit.confidence_level) end
-    nothing
-end
-function lsqfit!(fit::FitFunction, xdata::Array{<:Real, 1}, ydata::Array{<:Real, 1}, err::Array{<:Real, 1}; estimate_uncertainties=false)::Nothing
-    w = inv.(err)
-    fr = LsqFit.curve_fit(fit.model, xdata, ydata, w, collect(Float64, fit.initial_parameters))
-    fit.parameters = typeof(fit.initial_parameters)(fr.param)
-    if estimate_uncertainties fit.uncertainties = LsqFit.margin_error(fr, 1 - fit.confidence_level) end
-    nothing
-end
-function lsqfit!(fit::FitFunction, xdata::Array{<:Real, 1}, ydata::Array{<:Real, 1}, xerr::Array{<:Real, 1}, yerr::Array{<:Real, 1}; estimate_uncertainties=false)::Nothing
-    w = @. 1 / sqrt(xerr^2 + yerr^2)
-    fr = LsqFit.curve_fit(fit.model, xdata, ydata, w, collect(Float64, fit.initial_parameters))
-    fit.parameters = typeof(fit.initial_parameters)(fr.param)
-    if estimate_uncertainties fit.uncertainties = LsqFit.margin_error(fr, 1 - fit.confidence_level) end
-    nothing
+function _leastsquare(ydata::Vector{T}, ymodel::Vector{T}, weights::Vector{T})::T where {T <: AbstractFloat}
+    ls::T = 0
+    @inbounds for i in eachindex(ydata)
+        ls += (ydata[i] - ymodel[i])^2 * weights[i]
+    end
+    return ls
 end
 
+function lsqfit!(fit::FitFunction{T, 1, NP}, xdata::Vector{T}, ydata::Vector{T}; weights::Vector{T} = ones(T, length(xdata))) where {T <: AbstractFloat, NP}
+    function lsq(params::Vector{T})::T
+        ymodel::Vector{T} = fit.model(xdata, params)::Vector{T}
+        ls::T = _leastsquare( ydata, ymodel, weights )
+        return ls
+    end 
+    optim_result = try
+        optimize( lsq, fit.initial_parameters) 
+    catch err
+        optimize( lsq, fit.initial_parameters, BFGS())
+    end
+    _set_fitted_parameters!(fit, optim_result.minimizer)
+end
+
+function lsqfit!(fit::FitFunction{T, 1, NP}, xdata::Vector{T}, ydata::Vector{T}, xerr::Vector{T}, yerr::Vector{T}) where {T <: AbstractFloat, NP}
+    weights::Vector{T} = @. 1 / sqrt(xerr^2 + yerr^2)
+    return lsqfit!(fit, xdata, ydata, weights = weights)
+end
+
+function lsqfit!(fit::FitFunction{T, 1, NP}, xdata::Vector{T}, ydata::Vector{T}, err::Vector{T}) where {T <: AbstractFloat, NP}
+    weights::Vector{T} = inv.(err)
+    return lsqfit!(fit, xdata, ydata, weights = weights)
+end
+
+
 """
-    lsqfit!(fit::FitFunction, h::Histogram; estimate_uncertainties=false)::Nothing
+    lsqfit!(fit::FitFunction{T, 1, NP}, h::Histogram)::Nothing where {T <: AbstractFloat, NP}
 
 Performs a Least Square Fit with the model `fit.model` and the initial parameters `fit.initial_parameters`
-on the histogram `h` in the range `fit.fitrange`. The determined parameters are stored in `fit.parameters`
-and the corresponding uncertainties in `fit.uncertainties` for the given confidence level `fit.confidence_level`.
-
-The uncertainties are marginalizations of the covariance matrix determined by the LSQFit.jl package.
-They are only calculated if the keywort `estimate_uncertainties` is set to `true`.
-
-See [LsqFit.jl](https://github.com/JuliaNLSolvers/LsqFit.jl) for more detail.
+on the histogram `h` in the range `fit.fitranges[1]`. The determined parameters are stored in `fit.fitted_parameters`.
 """
-function lsqfit!(fit::FitFunction, h::Histogram; estimate_uncertainties=false)::Nothing
-    first_bin::Int = StatsBase.binindex(h, first(fit.fitrange))
-    last_bin::Int  = StatsBase.binindex(h, last(fit.fitrange))
+function lsqfit!(fit::FitFunction{T, 1, NP}, h::Histogram)::Nothing where {T <: AbstractFloat, NP}
+    first_bin::Int = StatsBase.binindex(h, first(fit.fitranges[1]))
+    last_bin::Int  = StatsBase.binindex(h, last(fit.fitranges[1]))
     if first_bin < 1 first_bin = 1 end
     if (last_bin > length(h.weights)) last_bin = length(h.weights) end
-    x::Array{Float64,1} = collect(h.edges[1])[first_bin:last_bin] .+ 0.5 * step(h.edges[1])
-    y::Array{Float64,1} = h.weights[first_bin:last_bin]
-    yerr = sqrt.(y) # Poisson distributed
-    yerr = [ ye != 0 ? ye : 1.  for ye in yerr] 
-    fr = LsqFit.curve_fit(fit.model, x, y, yerr, collect(Float64, fit.initial_parameters))
-    fit.parameters = typeof(fr.initial_parameters)(fr.param)
-    if estimate_uncertainties fit.uncertainties = LsqFit.margin_error(fr, 1 - fit.confidence_level) end
-    nothing
+    bin_centers::Vector{T} = StatsBase.midpoints(h.edges[1])[first_bin:last_bin]
+    counts::Vector{T} = h.weights[first_bin:last_bin]
+    weights = sqrt.(counts) # Poisson distributed
+    weights = [ w != 0 ? w : 1.  for w in weights] 
+
+    lsqfit!(fit, bin_centers, counts, weights)
 end
 
 
--- src/Fitting/plotrecipes.jl
+++
@@ -1,8 +0,0 @@
-@recipe function f(ff::FitFunction; npoints = 501, use_initial_parameters = false)
-    x = collect(range(ff.fitrange[1], stop=ff.fitrange[2], length=npoints))
-    par = use_initial_parameters ? ff.initial_parameters : ff.parameters
-    y = ff.model(x, par)
-    linecolor --> :red
-    x,y
-end
-
