b6a8fa2c821f61bf141cda24e49d770e8524254d
schuster <schuster@mpp.mpg.de>
2020-02-17 15:17:28 +0100

    Added a Linear function to the pool of predefined functions

--- src/Fitting/general_model_functions.jl
+++ src/Fitting/general_model_functions.jl
@@ -2,14 +2,14 @@ FitFunction(s::Symbol) = FitFunction(Float64, Val(s))
 FitFunction(T::Type{<:AbstractFloat}, s::Symbol) = FitFunction(T, Val(s))
 
 function FitFunction(T::Type{<:AbstractFloat}, s::Val{:Gauss})
-    ff = FitFunction{T}( Gauss, 1, 3 ) 
+    ff = FitFunction{T}( Gauss, 1, 3 )
     set_parameter_names!(ff, ["A", "σ", "μ"])
     set_initial_parameters!(ff, [1, 1, 0])
     set_fitranges!(ff, ([-1, 1],))
     return ff
 end
 function FitFunction(T::Type{<:AbstractFloat}, s::Val{:PDF_Gauss})
-    ff = FitFunction{T}( PDF_Gauss, 1, 2 ) 
+    ff = FitFunction{T}( PDF_Gauss, 1, 2 )
     set_parameter_names!(ff, ["σ", "μ"])
     set_initial_parameters!(ff, [1, 0])
     set_fitranges!(ff, ([-1, 1],))
@@ -17,19 +17,28 @@ function FitFunction(T::Type{<:AbstractFloat}, s::Val{:PDF_Gauss})
 end
 
 function FitFunction(T::Type{<:AbstractFloat}, s::Val{:GaussPlusConstantBackground})
-    ff = FitFunction{T}( Gauss_plus_const_background, 1, 4 ) 
+    ff = FitFunction{T}( Gauss_plus_const_background, 1, 4 )
     set_parameter_names!(ff, ["A", "σ", "μ", "offset"])
     set_initial_parameters!(ff, [1, 1, 0, 1])
     set_fitranges!(ff, ([-1, 1],))
     return ff
 end
 function FitFunction(T::Type{<:AbstractFloat}, s::Val{:GaussPlusLinearBackground})
-    ff = FitFunction{T}( Gauss_plus_linear_background, 1, 5 ) 
+    ff = FitFunction{T}( Gauss_plus_linear_background, 1, 5 )
     set_parameter_names!(ff, ["A", "σ", "μ", "offset", "lin. slope"])
     set_initial_parameters!(ff, [1, 1, 0, 1, 0])
     set_fitranges!(ff, ([-1, 1],))
     return ff
 end
+
+function FitFunction(T::Type{<:AbstractFloat}, s::Val{:Linear})
+    ff = FitFunction{T}( Linear, 1, 2 )
+    set_parameter_names!(ff, ["offset", "slope"])
+    set_initial_parameters!(ff, [0, 0])
+    set_fitranges!(ff, ([-1, 1],))
+    return ff
+end
+
 
 function min_0(x::T) where {T <: Real}
     return x < 0 ? zero(typeof(x)) : x
@@ -40,10 +49,16 @@ end
 
 function get_inf(x::T) where {T <: Real}
     return T(Inf)
-end 
-function get_inf(x) 
+end
+function get_inf(x)
     return get_inf.(x)
-end 
+end
+
+function Linear(x, p)
+    offset  = p[1]
+    slope   = p[2]
+    return @fastmath @. offset + slope * x
+end
 
 function Gauss(x, p)
     scale = p[1]
