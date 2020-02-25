0fca89df204f7577dc31dd5fc9be4c5a73cbcc2c
schuster <schuster@mpp.mpg.de>
2020-02-25 16:22:33 +0100

    Added possibility to pass custom weights/Uncertainties to histogram fit. Standard remains Poisson.

--- src/Fitting/lsqfit.jl
+++ src/Fitting/lsqfit.jl
@@ -1,17 +1,12 @@
-function lsqfit!(fit::FitFunction{T, 1, NP}, xdata::Vector{T}, ydata::Vector{T}; weights::Vector{T} = ones(T, length(xdata)), force_fit =false, kwargs...) where {T <: AbstractFloat, NP}
+function lsqfit!(fit::FitFunction{T, 1, NP}, xdata::Vector{T}, ydata::Vector{T}; weights::Vector{T} = ones(T, length(xdata)), kwargs...) where {T <: AbstractFloat, NP}
     fit.fitranges == ([-Inf, Inf],) ? set_fitranges!(fit, ((xdata[1], xdata[end]), )) : nothing
     weights = map(x-> isnan(x) || isinf(x) ? 1.0 : x, weights)
-    if fit.model == RadiationSpectra.Linear && force_fit == false
-        @info("Using Linear Regression instead of LsqFit.")
-        linear_regression!(fit, xdata, ydata, weights = weights, kwargs...)
-    else
-        f = curve_fit(fit.model, xdata, ydata, weights, fit.initial_parameters; kwargs...)
-        set_fit_backend_result!(fit, f)
-        _set_fitted_parameters!(fit, f.param)
-        _set_residuals!(fit, fit.backend_result.resid)
-        _set_Χ²!(fit, weights)
-        fit
-    end
+    f = curve_fit(fit.model, xdata, ydata, weights, fit.initial_parameters; kwargs...)
+    set_fit_backend_result!(fit, f)
+    _set_fitted_parameters!(fit, f.param)
+    _set_residuals!(fit, fit.backend_result.resid)
+    _set_Χ²!(fit, weights)
+    fit
 end
 
 function lsqfit!(fit::FitFunction{T, 1, NP}, xdata::Vector{T}, ydata::Vector{T}, xerr::Vector{T}, yerr::Vector{T}; kwargs...) where {T <: AbstractFloat, NP}
@@ -31,14 +26,14 @@ end
 Performs a least square fit with the model `fit.model` and the initial parameters `fit.initial_parameters`
 on the histogram `h` in the range `fit.fitranges[1]`. The determined parameters are stored in `fit.fitted_parameters`.
 """
-function lsqfit!(fit::FitFunction{T, 1, NP}, h::Histogram) where {T <: AbstractFloat, NP}
+function lsqfit!(fit::FitFunction{T, 1, NP}, h::Histogram; weights::Union{Missing, Vector{T}} = missing) where {T <: AbstractFloat, NP}
     first_bin::Int = StatsBase.binindex(h, first(fit.fitranges[1]))
     last_bin::Int  = StatsBase.binindex(h, last(fit.fitranges[1]))
     if first_bin < 1 first_bin = 1 end
     if (last_bin > length(h.weights)) last_bin = length(h.weights) end
     bin_centers::Vector{T} = StatsBase.midpoints(h.edges[1])[first_bin:last_bin]
     counts::Vector{T} = h.weights[first_bin:last_bin]
-    err::Vector{T} = sqrt.(counts) # Poisson distributed
+    err::Vector{T} = ismissing(weights) ? sqrt.(counts) : weights[first_bin:last_bin] # Assume Poisson statistics if no weights are provided
     err = [ w != 0 ? w : 1.  for w in err]
 
     lowerbounds = map(b->b.left,  fit.parameter_bounds)
