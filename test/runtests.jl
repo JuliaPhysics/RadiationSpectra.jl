22310c106c60216d6cca4db7d61c309392d841e1
Lukas Hauertmann <lhauert@mpp.mpg.de>
2020-03-16 14:51:20 +0100

    Remove BAT.jl from Docs and Tests since
    it depends on the master branch of BAT.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 # This file is a part of RadiationSpectra.jl, licensed under the MIT License (MIT).
 using Test
 
-ENV["JULIA_DEBUG"] = "BAT"
-using BAT
+# ENV["JULIA_DEBUG"] = "BAT"
+# using BAT
 using RadiationSpectra
 using IntervalSets
 
@@ -46,22 +46,22 @@ using IntervalSets
         @test abs(fitted_pars[3] - 1460.830) <= 3
     end
 
-    if @isdefined BAT
-        ff_bat = FitFunction(T, :GaussPlusLinearBackground)
-        set_fitranges!(ff_bat, ((1461 - 20, 1461 + 20),))
-        set_parameter_bounds!(ff_bat, [0.0..10^7, 0.2..2.0, 1455..1465, 0..10^4, -30..30])
+    # if @isdefined BAT
+    #     ff_bat = FitFunction(T, :GaussPlusLinearBackground)
+    #     set_fitranges!(ff_bat, ((1461 - 20, 1461 + 20),))
+    #     set_parameter_bounds!(ff_bat, [0.0..10^7, 0.2..2.0, 1455..1465, 0..10^4, -30..30])
 
-        batfit!(ff_bat, h_cal)
-        # get_histogram_pdf(ff_bat, nbins = 3)
-        # get_standard_deviations(ff_bat)
-        # get_samples_inds_by_chain_id(ff_bat, 1)
+    #     batfit!(ff_bat, h_cal)
+    #     # get_histogram_pdf(ff_bat, nbins = 3)
+    #     # get_standard_deviations(ff_bat)
+    #     # get_samples_inds_by_chain_id(ff_bat, 1)
         
-        fitted_pars = get_fitted_parameters(ff_bat)
-        @show ff_bat
-        @testset "BAT Fit" begin
-            @test abs(fitted_pars[3] - 1460.830) <= 3
-        end
-    end
+    #     fitted_pars = get_fitted_parameters(ff_bat)
+    #     @show ff_bat
+    #     @testset "BAT Fit" begin
+    #         @test abs(fitted_pars[3] - 1460.830) <= 3
+    #     end
+    # end
 
 
     @testset "General model functions" begin
