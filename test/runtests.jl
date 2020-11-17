87c7557722655a423df8b3e3f22b98069bf45feb
Lukas Hauertmann <lhauert@mpp.mpg.de>
2020-11-17 16:00:44 +0100

    remove BAT from tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 # This file is a part of RadiationSpectra.jl, licensed under the MIT License (MIT).
 using Test
 
-using BAT
+# using BAT
 using RadiationSpectra
 using IntervalSets
 
@@ -45,19 +45,19 @@ using IntervalSets
         @test abs(fitted_pars[3] - 1460.830) <= 3
     end
 
-    if @isdefined BAT
-        ff_bat = FitFunction(T, :GaussPlusLinearBackground)
-        set_fitranges!(ff_bat, ((1461 - 20, 1461 + 20),))
-        set_parameter_bounds!(ff_bat, [0.0..10^7, 0.2..2.0, 1455..1465, 0..10^4, -30..30])
+    # if @isdefined BAT
+    #     ff_bat = FitFunction(T, :GaussPlusLinearBackground)
+    #     set_fitranges!(ff_bat, ((1461 - 20, 1461 + 20),))
+    #     set_parameter_bounds!(ff_bat, [0.0..10^7, 0.2..2.0, 1455..1465, 0..10^4, -30..30])
 
-        batfit!(ff_bat, h_cal)
+    #     batfit!(ff_bat, h_cal)
 
-        fitted_pars = get_fitted_parameters(ff_bat)
-        @show ff_bat
-        @testset "BAT Fit" begin
-            @test abs(fitted_pars[3] - 1460.830) <= 3
-        end
-    end
+    #     fitted_pars = get_fitted_parameters(ff_bat)
+    #     @show ff_bat
+    #     @testset "BAT Fit" begin
+    #         @test abs(fitted_pars[3] - 1460.830) <= 3
+    #     end
+    # end
 
 
     @testset "General model functions" begin
