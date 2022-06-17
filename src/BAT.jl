4b62a1067393b86ec18b0fe2a4a6302c875f6183
Lukas Hauertmann <lhauert@mpp.mpg.de>
2022-06-17 10:13:47 +0200

    Add keywords to `bat_fit` for more flexibility

--- src/BAT.jl
+++ src/BAT.jl
@@ -11,30 +11,45 @@ function bat_fit(UvD::Type{<:UvSpectrumDensity}, h::Histogram{<:Any, 1};
             nsteps_init::Int = 2000, 
             nsteps_per_cycle::Int = 2000, 
             strict = true,
+            mcmcalgo = BAT.HamiltonianMC(),
+            convergence = missing, 
+            init = missing, 
+            burnin = missing, 
             max_ncycles::Int = 1, 
-            threshold::Real = 1.1) 
-    hp = HistLLHPrecalulations(h)
+            threshold::Real = 1.1
+)
+    hp = HistLLHPrecalulations(float(h))
     sd = SpectrumDensity{typeof(hp), UvD}(hp)
     posterior = BAT.PosteriorDensity(sd, ismissing(prior) ? BAT.NamedTupleDist(bounds) : prior )
     posterior_transform = BAT.bat_transform(BAT.PriorToGaussian(), posterior, BAT.PriorSubstitution()).result
     trafo = BAT.trafoof(posterior_transform.likelihood) 
 
-    mcmcalgo = BAT.HamiltonianMC()
+    convergence = if ismissing(convergence)
+        BAT.BrooksGelmanConvergence(
+            threshold = threshold,
+            corrected = false
+        )
+    else
+        convergence
+    end
 
-    convergence = BAT.BrooksGelmanConvergence(
-        threshold = threshold,
-        corrected = false
-    )
+    init = if ismissing(init)
+        BAT.MCMCChainPoolInit(
+            init_tries_per_chain = 8..128,
+            nsteps_init = nsteps_init,
+        )
+    else
+        init
+    end
 
-    init = BAT.MCMCChainPoolInit(
-        init_tries_per_chain = 8..128,
-        nsteps_init = nsteps_init,
-    )
-
-    burnin = BAT.MCMCMultiCycleBurnin( 
-        nsteps_per_cycle = nsteps_per_cycle, 
-        max_ncycles = max_ncycles
-    )
+    burnin = if ismissing(burnin)
+        BAT.MCMCMultiCycleBurnin( 
+            nsteps_per_cycle = nsteps_per_cycle, 
+            max_ncycles = max_ncycles
+        )
+    else
+        burnin
+    end
 
     bat_samples = inv(trafo).(
             BAT.bat_sample( 
